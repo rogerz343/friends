7996c63cf29e3e753f52e1636b8effc19b458023
rogerz343 <rogerz888@gmail.com>
2018-05-20 01:15:47 -0400

    harvester simplified

--- friends-graph/src/Harvester.java
+++ friends-graph/src/Harvester.java
@@ -1,129 +1,82 @@
 import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
 
 /**
- * 
+ * A class used to download html documents corresponding to a facebook user's "Friends" page,
+ * with all of the html elements loaded.
  * @author roger
  * Assumptions made before executing code:
- * - facebook banner, chrome console, chrome scrollbar colors are correct
+ * - FRIENDS_BUTTON_COLOR is correct
  * - the display is a 1920 x 1080, running windows with 100% scaling
  * - the windows taskbar is at the default height
  * - chrome is maximized on the screen
  * - chrome's bookmark bar is open
  * - chrome's zoom level is default (100%)
- * - chrome's downloads bar (at the bottom of the page) is closed
+ * - chrome's downloads bar (at the bottom of the page) is OPEN
+ * - chrome's developer tools panel is CLOSED
+ * - the current page is the main page of a facebook profile, scrolled all the way to the top
  */
 public class Harvester {
     
-    private Robot robot;
-    private static int FB_BANNER_X = 1840;
-    private static int FB_BANNER_Y = 100;
-    private static Color FB_BANNER_COLOR = new Color(66, 103, 178);
-    private static Color CONSOLE_COLOR = new Color(243, 243, 243);
+    private InterruptibleRobot robot;
     
-    private static int FRIENDS_X = 735;
+    private static int FRIENDS_X = 842; // 735;
     private static int FRIENDS_Y_COVER = 460;
     private static int FRIENDS_Y_NO_COVER = 360;
     private static Color FRIENDS_BUTTON_COLOR = new Color(255, 255, 255);
     
     private static int SCROLLBAR_X = 1910;
-    private static int SCROLLBAR_Y = 1020;
+    private static int SCROLLBAR_Y = 972; // 1020 if downloads tab is closed
     private static Color SCROLLBAR_NOT_BOTTOM = new Color(241, 241, 241);
     private static Color SCROLLBAR_BOTTOM = new Color(193, 193, 193);
     
-    private static int ELEMENTS_TAB_X = 1460;
-    private static int ELEMENTS_TAB_Y = 100;
-    private static int HTML_HEADER_X = 1390;
-    private static int HTML_HEADER_Y = 143;
-    private static int EDIT_AS_HTML_X = 1410;
-    private static int EDIT_AS_HTML_Y = 183;
-    
+    private static int EMPTY_SPACE_X = 200;
+    private static int EMPTY_SPACE_Y = 300;    
     
     public Harvester() throws AWTException {
-        robot = new Robot();
+        robot = new InterruptibleRobot();
         robot.setAutoWaitForIdle(true);
         robot.setAutoDelay(100);
     }
     
     // TODO: add error checking
-    public int harvest() {
-        closeConsole();
+    /**
+     * Downloads the html document of a facebook user's "Friends" page. See class documentation
+     * for more detail.
+     */
+    public void harvest() {
         viewFriendsPage();
+        // give the page some time to load
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
         scrollToBottom();
-        String html = fetchHtmlString();
-    }
-    
-    /**
-     * Closes the chrome console, if it is open.
-     * @return 0 if no error occurred. 1 otherwise.
-     */
-    private int closeConsole() {
-        robot.mouseMove(0, 0);
-        Color sample = robot.getPixelColor(FB_BANNER_X, FB_BANNER_Y);
-        if (sample.equals(FB_BANNER_COLOR)) {
-            return 0;
-        }
-        if (sample.equals(CONSOLE_COLOR)) {
-            robot.keyPress(KeyEvent.VK_F12);
-            robot.keyRelease(KeyEvent.VK_F12);
-            sample = robot.getPixelColor(FB_BANNER_X, FB_BANNER_Y);
-            if (sample.equals(FB_BANNER_COLOR)) {
-                return 0;
-            }
-        }
-        return 1;
-    }
-    
-    /**
-     * Opens the chrome console, if it is closed.
-     * @return 0 if no error occurred. 1 otherwise.
-     */
-    private int openConsole() {
-        robot.mouseMove(0, 0);
-        Color sample = robot.getPixelColor(FB_BANNER_X, FB_BANNER_Y);
-        if (sample.equals(CONSOLE_COLOR)) {
-            return 0;
-        }
-        if (sample.equals(FB_BANNER_COLOR)) {
-            robot.keyPress(KeyEvent.VK_F12);
-            robot.keyRelease(KeyEvent.VK_F12);
-            sample = robot.getPixelColor(FB_BANNER_X, FB_BANNER_Y);
-            if (sample.equals(CONSOLE_COLOR)) {
-                return 0;
-            }
-        }
-        return 1;
+        fetchHtml();
     }
     
     /**
      * Assumes that the current page is the main page for a specific person
      * and navigates to friends page
      * @return 0 if no error occurred. 1 if could not locate friends page button.
+     * 2 if other error occured.
      */
     private int viewFriendsPage() {
+        if (robot.interrupted) { return 2; } 
         robot.mouseMove(0, 0);
-        Color sample1 = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_NO_COVER);
-        Color sample2 = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_NO_COVER - 10);
-        Color sample3 = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_NO_COVER + 10);
-        if (sample1.equals(FRIENDS_BUTTON_COLOR)
-                && sample2.equals(FRIENDS_BUTTON_COLOR)
-                && sample3.equals(FRIENDS_BUTTON_COLOR)) {
+        Color sample = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_NO_COVER);
+        if (sample.equals(FRIENDS_BUTTON_COLOR)) {
             robot.mouseMove(FRIENDS_X, FRIENDS_Y_NO_COVER);
         } else {
-            sample1 = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_COVER);
-            sample2 = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_COVER - 10);
-            sample3 = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_COVER + 10);
-            if (sample1.equals(FRIENDS_BUTTON_COLOR)
-                    && sample2.equals(FRIENDS_BUTTON_COLOR)
-                    && sample3.equals(FRIENDS_BUTTON_COLOR)) {
+            sample = robot.getPixelColor(FRIENDS_X, FRIENDS_Y_COVER);
+            if (sample.equals(FRIENDS_BUTTON_COLOR)) {
                 robot.mouseMove(FRIENDS_X, FRIENDS_Y_NO_COVER);
             } else {
                 return 1;
@@ -131,11 +84,6 @@ public class Harvester {
         }
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         return 0;
     }
     
@@ -144,10 +92,10 @@ public class Harvester {
      * @return 0 if no error occurred. 1 otherwise.
      */
     private int scrollToBottom() {
-        closeConsole();
         robot.mouseMove(0, 0);
         boolean done = false;
         while (!done) {
+            if (robot.interrupted) { return 1; }
             robot.keyPress(KeyEvent.VK_PAGE_DOWN);
             robot.keyRelease(KeyEvent.VK_PAGE_DOWN);
             robot.keyPress(KeyEvent.VK_PAGE_DOWN);
@@ -165,7 +113,6 @@ public class Harvester {
                 try {
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
                 sample = robot.getPixelColor(SCROLLBAR_X, SCROLLBAR_Y);
@@ -180,50 +127,105 @@ public class Harvester {
     }
     
     /**
-     * Returns the dynamically generated html from the current web page. Assumes that the
-     * chrome is in focus and on the correct webpage.
-     * @return The html string if it worked. undefined behavior if an error occured.
+     * Saves the dynamically generated html file from the current web page as an html file
+     * in the current working directory. Assumes that chrome is in focus and is on the correct webpage.
+     * Assumes that chrome's save "Webpage, complete" saves the dynamically generated html (not the source code).
+     * @return 0 if no error occurred. 1 otherwise.
      */
-    private String fetchHtmlString() {
-        openConsole();
-        robot.mouseMove(ELEMENTS_TAB_X, ELEMENTS_TAB_Y);
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        
-        robot.mouseMove(HTML_HEADER_X, HTML_HEADER_Y);
+    private int fetchHtml() {
+        if (robot.interrupted) { return 1; } 
+        robot.mouseMove(EMPTY_SPACE_X, EMPTY_SPACE_Y);
         robot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
         robot.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
         
-        robot.mouseMove(EDIT_AS_HTML_X, EDIT_AS_HTML_Y);
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        
+        if (robot.interrupted) { return 1; }
         robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_A);
-        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyRelease(KeyEvent.VK_S);
         robot.keyRelease(KeyEvent.VK_CONTROL);
         
-        // TOOD: make this not slow af
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
+        // change the name of the file just in case, to avoid overriding message
+        if (robot.interrupted) { return 1; }
+        robot.keyPress(KeyEvent.VK_HOME);
+        robot.keyRelease(KeyEvent.VK_HOME);
+        typeRandomDigit();
+        typeRandomDigit();
+        typeRandomDigit();
+        typeRandomDigit();
+        typeRandomDigit();
+        
+        if (robot.interrupted) { return 1; }
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.keyPress(KeyEvent.VK_DOWN);
+        robot.keyRelease(KeyEvent.VK_DOWN);
+        robot.keyPress(KeyEvent.VK_DOWN);
+        robot.keyRelease(KeyEvent.VK_DOWN);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        return 0;
+    }
+    
+    private void typeRandomDigit() {
+        int random = (int) (Math.random() * 10);
+        int code = KeyEvent.VK_0;
+        if (random == 0) { code = KeyEvent.VK_0; }
+        else if (random == 1) { code = KeyEvent.VK_1; }
+        else if (random == 2) { code = KeyEvent.VK_2; }
+        else if (random == 3) { code = KeyEvent.VK_3; }
+        else if (random == 4) { code = KeyEvent.VK_4; }
+        else if (random == 5) { code = KeyEvent.VK_5; }
+        else if (random == 6) { code = KeyEvent.VK_6; }
+        else if (random == 7) { code = KeyEvent.VK_7; }
+        else if (random == 8) { code = KeyEvent.VK_8; }
+        else if (random == 9) { code = KeyEvent.VK_9; }
+        else { System.out.println("Math is broken"); }
+        robot.keyPress(code);
+        robot.keyRelease(code);
+    }
+    
+    /**
+     * A java Robot that monitors whether it has been interrupted by user input.
+     * @author roger
+     *
+     */
+    private class InterruptibleRobot extends Robot {
+        
+        private int lastXMove;
+        private int lastYMove;
+        public boolean interrupted;
+
+        public InterruptibleRobot() throws AWTException {
+            super();
+            lastXMove = -1;
+            lastYMove = -1;
+            interrupted = false;
         }
         
-        robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_C);
-        robot.keyRelease(KeyEvent.VK_C);
-        robot.keyRelease(KeyEvent.VK_CONTROL);
-        
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        String html = null;
-        try {
-            html = (String) clipboard.getData(DataFlavor.stringFlavor);
-        } catch (UnsupportedFlavorException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public void mouseMove(int x, int y) {
+            checkInterrupt();
+            super.mouseMove(x, y);
+            lastXMove = x;
+            lastYMove = y;
         }
-        return html;
+        
+        public void keyPress(int keycode) {
+            checkInterrupt();
+            super.keyPress(keycode);
+        }
+        
+        /**
+         * Checks if the user (or any other program) has interrupted the robot by checking
+         * if the mouse cursor is still in the same position that it was last mouseMove'ed to.
+         * If interrupted, the instance variable `interrupted` is set to true.
+         */
+        private void checkInterrupt() {
+            if (lastXMove == -1 || lastYMove == -1) { return; }
+            Point curr = MouseInfo.getPointerInfo().getLocation();
+            interrupted = curr.x != lastXMove || curr.y != lastYMove;
+        }
+        
     }
 }
--- friends-graph/src/Main.java
+++ friends-graph/src/Main.java
@@ -1,9 +1,25 @@
+import java.awt.AWTException;
 
+/**
+ * friends
+ * @author roger
+ * Program used to fetch and analyze information about a user's facebook friends. See the
+ * readme.txt for more details.
+ */
 public class Main {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    // TODO: add a GUI
+    public static void main(String[] args) throws AWTException {
+        harvest();
     }
-
+    
+    public static void harvest() throws AWTException {
+        // give the user some time to set up the facebook page correctly
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Harvester h = new Harvester();
+        h.harvest();
+    }
 }
